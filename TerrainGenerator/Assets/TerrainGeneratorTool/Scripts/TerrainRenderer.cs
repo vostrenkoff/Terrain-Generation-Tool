9a2bcc163e05b86f3593e0a941916a97ee381382
vostrenkoff <iseyth@gmail.com>
2023-03-31 12:19:31 +0200

    v1.0

---
+++ TerrainGenerator/Assets/TerrainGeneratorTool/Scripts/TerrainRenderer.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TerrainGen
+{
+    public class TerrainRenderer : MonoBehaviour
+    {
+        private MeshRenderer meshRenderer;
+        private MeshFilter meshFilter;
+        private Mesh mesh;
+
+        public float _terrainHeight;
+        public Texture2D _heightMap;
+        public Shader shader;
+
+        void Start()
+        {
+            if (_heightMap == null)
+            {
+                Debug.Log("Heightmap is not assigned!");
+                return;
+            }
+
+            meshRenderer = gameObject.AddComponent<MeshRenderer>();
+            meshRenderer.sharedMaterial = new Material(shader);
+            meshFilter = gameObject.AddComponent<MeshFilter>();
+
+            mesh = new Mesh();
+            mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
+            getMeshData(out Vector3[] vertices, out Color[] colors, out int[] triangles);
+            mesh.SetVertices(vertices);
+            mesh.SetTriangles(triangles, 0);
+            mesh.RecalculateNormals();
+            mesh.SetColors(colors);
+            meshFilter.mesh = mesh;
+            meshRenderer.sharedMaterial.SetFloat("vMultiplier", _terrainHeight);
+        }
+
+        void Update()
+        {
+            updateMeshData();
+        }
+
+        void updateMeshData()
+        {
+            meshRenderer.sharedMaterial.SetFloat("vMultiplier", _terrainHeight);
+            meshFilter.mesh.RecalculateNormals();
+        }
+
+        void getMeshData(out Vector3[] vertices, out Color[] colors, out int[] triangles)
+        {
+            vertices = new Vector3[_heightMap.height * _heightMap.width];
+            colors = new Color[_heightMap.height * _heightMap.width];
+            List<int> trianglesList = new List<int>();
+
+            for (int y = 0; y < _heightMap.height; y++)
+            {
+                for (int x = 0; x < _heightMap.width; x++)
+                {
+                    float vertexHeight = _heightMap.GetPixel(x, y).grayscale;
+                    vertices[x + y * _heightMap.width] = new Vector3(x - _heightMap.width / 2, vertexHeight, y - _heightMap.height / 2);
+
+                    colors[x + y * _heightMap.width] = new Color(Mathf.Clamp(2f * vertexHeight, 0f, 1f), Mathf.Clamp(2f * (1f - vertexHeight), 0f, 1f), Mathf.Pow(1f - vertexHeight, 3f));
+
+                    if (x > 0 && y > 0)
+                    {
+                        trianglesList.Add(x + (y - 1) * _heightMap.width);
+                        trianglesList.Add(x - 1 + (y - 1) * _heightMap.width);
+                        trianglesList.Add(x - 1 + y * _heightMap.width);
+
+                        trianglesList.Add(x - 1 + y * _heightMap.width);
+                        trianglesList.Add(x + y * _heightMap.width);
+                        trianglesList.Add(x + (y - 1) * _heightMap.width);
+                    }
+                }
+            }
+
+            triangles = trianglesList.ToArray();
+        }
+    }
+}
---
+++ TerrainGenerator/Assets/TerrainGeneratorTool/Shaders/VertexColorShader.shader
@@ -0,0 +1,72 @@
+﻿Shader "Custom/VertexColorShader"
+{
+
+	Properties{
+		vMultiplier("vMultiplier", float) = 0
+		_MinHeight("Min Height", Range(0,1000)) = 0
+		_MaxHeight("Max Height", Range(0,100000)) = 300
+		_ColorLow("Color Low", Color) = (0.3, 0.5, 0.2, 0.1)
+		_ColorMid("Color Mid", Color) = (0.45, 0.45, 0.45, 1)
+		_ColorHigh("Color High", Color) = (0.9, 0.9, 0.9, 1)
+		_Shininess("Shininess", Range(0.01, 1)) = 0
+		_Texture("Texture", 2D) = "white" {}
+		_SpecMap("Specular Map", 2D) = "white" {}
+	}
+		SubShader{
+			Tags { "RenderType" = "Opaque" }
+
+
+			LOD 200
+
+			CGPROGRAM
+			#pragma surface surf Lambert
+			#pragma vertex vert
+			#pragma target 3.0
+			float vMultiplier;
+
+			float _MinHeight;
+			float _MaxHeight;
+			float4 _Texture_ST;
+			sampler2D _Texture;
+			sampler2D _SpecMap;
+			float4 _ColorLow;
+			float4 _ColorMid;
+			float4 _ColorHigh;
+			float _Shininess;
+
+			struct Input {
+				float4 vertColor;
+				float2 uv_MainTex;
+				float3 worldPos;
+			};
+
+			void vert(inout appdata_full vertex, out Input surfInput) {
+				UNITY_INITIALIZE_OUTPUT(Input, surfInput);
+				vertex.vertex.y *= vMultiplier;
+				surfInput.vertColor = vertex.color;
+
+
+			}
+
+			void surf(Input IN, inout SurfaceOutput output) {
+				// calculate height ratio from vertex height
+					float height = IN.worldPos.y * vMultiplier;
+					float heightRatio = (height - _MinHeight) / (_MaxHeight - _MinHeight);
+					heightRatio = clamp(heightRatio, 0.0, 1.0);
+
+					// set surface color based on height ratio
+					if (heightRatio < 0.23) {
+						output.Albedo = lerp(_ColorLow.rgb, _ColorMid.rgb, heightRatio * 2);
+						
+					}
+					else if (heightRatio < 0.76) {
+						output.Albedo = lerp(_ColorMid.rgb, _ColorHigh.rgb, (heightRatio - 0.33) * 3);
+					}
+					else {
+						output.Albedo = _ColorHigh.rgb;
+					}
+			}
+			ENDCG
+	}
+		FallBack "Diffuse"
+}
